78399128ba61d9b71933e36869ff338c45aceaab
Andy Armstrong <andy@hexten.net>
2020-06-28 20:06:13 +0100

    Test coverage.

--- test/unit/tools/functions.js
+++ test/unit/tools/functions.js
@@ -13,6 +13,73 @@ describe("kv", () => {
   });
 });
 
+describe("defined", () => {
+  it("should test definedness", () => {
+    [null, 0, "", true, "Hello", {}, undefined, "Bye!"]
+      .filter(F.defined)
+      .should.deepEqual([null, 0, "", true, "Hello", {}, "Bye!"]);
+  });
+});
+
+describe("sumObj", () => {
+  it("should sum object values", () => {
+    F.sumObj({ a: 1, b: 2, c: 3 }).should.equal(6);
+  });
+});
+
+describe("cmp", () => {
+  it("should sort naturally", () => {
+    [3, 1, 2, 4].sort(F.cmp).should.deepEqual([1, 2, 3, 4]);
+    ["C", "A", "B", "D"].sort(F.cmp).should.deepEqual(["A", "B", "C", "D"]);
+  });
+});
+
+describe("bumper", () => {
+  it("should bump", () => {
+    const o = { a: 12, b: { c: 17 } };
+    const b = F.bumper(o);
+    b("a");
+    b("b", "c");
+    b("d", "e");
+    b("a");
+    b("c");
+    b("d", "d", "f");
+    o.should.deepEqual({
+      a: 14,
+      b: { c: 18 },
+      d: { e: 1 },
+      c: 1,
+      d: { e: 1, d: { f: 1 } }
+    });
+  });
+});
+
+describe("sortKeysDeep", () => {
+  it("should sort keys", () => {
+    const o = {
+      z: [
+        { c: 1, a: 2, b: 3 },
+        { b: 1, c: 2, a: 3 }
+      ],
+      y: "Why?",
+      x: { m: 2, n: 3 }
+    };
+    const so = F.sortKeysDeep(o);
+    so.should.deepEqual(o);
+
+    const wso = {
+      x: { m: 2, n: 3 },
+      y: "Why?",
+      z: [
+        { a: 2, b: 3, c: 1 },
+        { a: 3, b: 1, c: 2 }
+      ]
+    };
+
+    JSON.stringify(so).should.equal(JSON.stringify(wso));
+  });
+});
+
 describe("combo", () => {
   it("should iterate combinations", () => {
     [...F.combo([])].should.deepEqual([[]]);
